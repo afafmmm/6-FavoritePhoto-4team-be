76aebb3f5215a2ea3031605c08ca062fdffba55d
root <root@DESKTOP-AFSEKMO.>
2025-05-15 17:38:39 +0900

    Fix: 함수에 model 매개변수 추가

--- src/utils/sort.js
+++ src/utils/sort.js
@@ -2,27 +2,32 @@
 repository에서 DB 자료 불러올 때:
 
 async function getAll(query = {}) {
-	const orderBy = getSort(query.orderBy)
+	const orderBy = getSort(query.orderBy) // (model, query)
 	...
 	
 	const cards = await prisma.photoCard.findMany({ orderBy, ... })
 */
 
-export default function getSort(order) {
-  const options = ["최신순", "높은 가격순", "낮은 가격순"];
+export default function getSort(model, order = "낮은 가격순") {
+  const modelType = {
+    photoCard: {
+      "낮은 가격순": { initialPrice: "asc" },
+      "높은 가격순": { initialPrice: "desc" },
+      최신순: { createdAt: "desc" },
+    },
+    userCard: {
+      "낮은 가격순": { price: "asc" },
+      "높은 가격순": { price: "desc" },
+      최신순: { createdAt: "desc" },
+    },
+  };
 
-  if (!options.includes(order)) {
-    throw new Error("최신순, 높은 가격순, 낮은 가격순 中 택1");
-  }
+  const sort = modelType[model];
 
-  switch (order) {
-    case "최신순":
-      return { createdAt: "desc" };
-    case "높은 가격순":
-      return { price: "desc" };
-    case "낮은 가격순":
-      return { price: "asc" };
-    default:
-      return { price: "asc" };
-  }
+  if (!sort) throw new Error("'PhotoCard'와 'UserCard' 中 택1");
+
+  if (!sort[order])
+    throw new Error("'최신순', '높은 가격순', '낮은 가격순' 中 택1");
+
+  return sort[order];
 }
