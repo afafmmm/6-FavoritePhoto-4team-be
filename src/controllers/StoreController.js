7fe3d4f41aca91dfc1da0390d8ee9216ff992aac
Wooju <dnwn810@naver.com>
2025-05-22 16:47:49 +0900

    feat: Store 전체 카드 목록 조회 API추가, 특정 포토카드 상세 조회 추가(후에 sale api 완성되면 api 변경 예정)

--- src/controllers/StoreController.js
+++ src/controllers/StoreController.js
@@ -1,1 +1,30 @@
-ㄴ;
+import express from "express";
+import storeService from "../services/StoreService.js";
+
+const storeController = express.Router();
+
+// 카드 목록 조회
+storeController.get("/", async (req, res, next) => {
+  try {
+    const cards = await storeService.getAllCards();
+    res.json(cards);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// 카드 상세 조회
+storeController.get("/cards/:id", async (req, res, next) => {
+  try {
+    const cardId = parseInt(req.params.id, 10);
+    const card = await storeService.getCardById(cardId);
+    if (!card) {
+      return res.status(404).json({ message: "카드를 찾을 수 없습니다." });
+    }
+    res.json(card);
+  } catch (err) {
+    next(err);
+  }
+});
+
+export default storeController;
